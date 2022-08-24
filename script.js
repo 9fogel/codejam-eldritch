aba72f7b1bbc28a4b55f88c0c01b32426dbc092c
Vera Kirayonak <Vera_Kirayonak@epam.com>
2022-08-25 01:01:08 +0300

    feat: add function to get initial set of cards for each anchient

--- script.js
+++ script.js
@@ -38,24 +38,113 @@ anchients.forEach((anchient) => {
       setAnchientScheme(azathoth);
       document.querySelector('.active').classList.remove('active');
       anchient.classList.add('active');
-      // console.log('Azathoth');
+      getCardArr(azathoth);
+      // const setofCards = getCardArr(azathoth);
+      // console.log(setofCards);
     } else if (anchient.innerHTML === 'Cthulthu') {
       setAnchientScheme(cthulhu);
       document.querySelector('.active').classList.remove('active');
       anchient.classList.add('active');
-      // console.log('Cthulthu');
+      getCardArr(cthulhu);
     } else if (anchient.innerHTML === 'IogSothoth') {
       setAnchientScheme(iogsothoth);
       document.querySelector('.active').classList.remove('active');
       anchient.classList.add('active');
-      // console.log('IogSothoth');
+      getCardArr(iogsothoth);
     } else if (anchient.innerHTML === 'ShubNiggurath') {
       setAnchientScheme(shubniggurath);
       document.querySelector('.active').classList.remove('active');
       anchient.classList.add('active');
-      // console.log('ShubNiggurath');
+      getCardArr(shubniggurath);
     }
   }
 });
+
+function getRandomNum(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+// function totalCards(arr) {
+//   let GBBTTotalArr = [];
+//   let GBBTotalObj = {};
+//   const totalGreen = arr[0] + arr[3] + arr[6];
+//   GBBTTotalArr.push(totalGreen);
+//   GBBTotalObj.green = totalGreen;
+//   // console.log(`total green - ${totalGreen}`);
+//   const totalBrown = arr[1] + arr[4] + arr[7];
+//   GBBTTotalArr.push(totalBrown);
+//   GBBTotalObj.brown = totalBrown;
+//   // console.log(`total green - ${totalBrown}`);
+//   const totalBlue = arr[2] + arr[5] + arr[8];
+//   GBBTTotalArr.push(totalBlue);
+//   GBBTotalObj.blue = totalBlue;
+//   // console.log(`total green - ${totalBlue}`);
+//   // console.log(GBBTTotalArr);
+//   // console.log(GBBTotalObj);
+//   return GBBTotalObj;
+// }
+
+
+function getCardArr(anchient) {
+  const totalGreen = anchient[0] + anchient[3] + anchient[6];
+  const totalBrown = anchient[1] + anchient[4] + anchient[7];
+  const totalBlue = anchient[2] + anchient[5] + anchient[8];
+
+  let arr = [];
+  let arrUnique = [];
+  let initCardArr = [];
+  let arrElem;
+
+  function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+}
+
+function fillArr (letter, start, end, number) {
+  while (arr.length < number) {
+    arrElem = getRandomIntInclusive(start, end);
+    if (!arr.includes(arrElem)) {
+      arr.push(arrElem);
+      arrUnique.push(`${letter}${arrElem}`);
+    }
+  }
+
+  initCardArr.push(...arrUnique); 
+  arr.length = 0;
+  arrUnique.length = 0;
+    
+}
+
+fillArr('green', 1, 18, totalGreen);
+fillArr('brown', 1, 21, totalBrown);
+fillArr('blue', 1, 12, totalBlue);
+
+console.log(initCardArr);
+return initCardArr;
+}
+
+
+
+
+
+
+// let randomNum = (getRandomNum(1, 21));
+
+const color = 'blue';
+const colorNum = color + 8;
+
+function showCard() {
+  const card = document.querySelector('.card-deck-flipped')
+  const bgImage = new Image();
+  bgImage.src = `./assets/MythicCards/${color}/${colorNum}.jpg`;
+  bgImage.onload = () => {
+    card.style.backgroundImage = `url(${bgImage.src})`;
+  }
+}
+
+showCard();
 
 
