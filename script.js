2987c388cbe002672490ef9193f966fabe839ec1
Vera Kirayonak <Vera_Kirayonak@epam.com>
2022-08-25 00:00:25 +0300

    feat: add mess btn and card deck HTML coding and styles

--- script.js
+++ script.js
@@ -1,88 +1,5 @@
-// const ancientsData = [
-//   {
-//     id: 'azathoth',
-//     name: 'azathoth',
-//     cardFace: Ancients.azathoth,
-//     firstStage: {
-//       greenCards: 1,
-//       blueCards: 1,
-//       brownCards: 2,
-//     },
-//     secondStage: {
-//       greenCards: 2,
-//       blueCards: 1,
-//       brownCards: 3,
-//     },
-//     thirdStage: {
-//       greenCards: 2,
-//       blueCards: 0,
-//       brownCards: 4,
-//     },
-//   },
-//   {
-//     id: 'cthulhu',
-//     name: 'cthulhu',
-//     cardFace: Ancients.cthulhu,
-//     firstStage: {
-//       greenCards: 0,
-//       blueCards: 2,
-//       brownCards: 2,
-//     },
-//     secondStage: {
-//       greenCards: 1,
-//       blueCards: 0,
-//       brownCards: 3,
-//     },
-//     thirdStage: {
-//       greenCards: 3,
-//       blueCards: 0,
-//       brownCards: 4,
-//     },
-//   },
-//   {
-//     id: 'iogSothoth',
-//     name: 'iogSothoth',
-//     cardFace: Ancients.iogSothoth,
-//     firstStage: {
-//       greenCards: 0,
-//       blueCards: 1,
-//       brownCards: 2,
-//     },
-//     secondStage: {
-//       greenCards: 2,
-//       blueCards: 1,
-//       brownCards: 3,
-//     },
-//     thirdStage: {
-//       greenCards: 3,
-//       blueCards: 0,
-//       brownCards: 4,
-//     },
-//   },
-//   {
-//     id: 'shubNiggurath',
-//     name: 'shubNiggurath',
-//     cardFace: Ancients.shubNiggurath,
-//     firstStage: {
-//       greenCards: 1,
-//       blueCards: 1,
-//       brownCards: 2,
-//     },
-//     secondStage: {
-//       greenCards: 3,
-//       blueCards: 1,
-//       brownCards: 2,
-//     },
-//     thirdStage: {
-//       greenCards: 2,
-//       blueCards: 0,
-//       brownCards: 4,
-//     },
-//   },
-// ]
-
 const anchients = document.querySelectorAll('.ancient-item');
-console.log(anchients);
+// console.log(anchients);
 
 const azathoth = [
   1, 2, 1,
@@ -121,22 +38,22 @@ anchients.forEach((anchient) => {
       setAnchientScheme(azathoth);
       document.querySelector('.active').classList.remove('active');
       anchient.classList.add('active');
-      console.log('Azathoth');
+      // console.log('Azathoth');
     } else if (anchient.innerHTML === 'Cthulthu') {
       setAnchientScheme(cthulhu);
       document.querySelector('.active').classList.remove('active');
       anchient.classList.add('active');
-      console.log('Cthulthu');
+      // console.log('Cthulthu');
     } else if (anchient.innerHTML === 'IogSothoth') {
       setAnchientScheme(iogsothoth);
       document.querySelector('.active').classList.remove('active');
       anchient.classList.add('active');
-      console.log('IogSothoth');
+      // console.log('IogSothoth');
     } else if (anchient.innerHTML === 'ShubNiggurath') {
       setAnchientScheme(shubniggurath);
       document.querySelector('.active').classList.remove('active');
       anchient.classList.add('active');
-      console.log('ShubNiggurath');
+      // console.log('ShubNiggurath');
     }
   }
 });
